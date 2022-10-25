789a084e00ce4a7b3def88e28df34d45b3a95781
sole88 <castreccinisoledad@gmail.com>
2022-10-24 22:57:23 -0300

    modificacion validacion, registro html exitoso

--- app.js
+++ app.js
@@ -6,12 +6,15 @@ document.getElementById("formulario").addEventListener("submit", (e) => {
   const telefono = document.getElementById("telefono");
   const mensaje = document.getElementById('mensaje');
 
+ 
   if (usuario.value === "") {
     document.getElementById("name").innerHTML="Por favor, escribe tu nombre de usuario."
     usuario.focus();
-    document.getElementById("submit").addEventListener("click", clean())
     return false;
-   } 
+   }
+   else{
+    document.getElementById("name").innerHTML=""
+   }
     
   if (email.value === "") {
     document.getElementById("mail").innerHTML="Por favor, escribe tu correo electrónico";
@@ -19,11 +22,14 @@ document.getElementById("formulario").addEventListener("submit", (e) => {
     return false;
   }
 
-  if (!emailValido(email.value)) {
+ else  if (!emailValido(email.value)) {
     document.getElementById("mail").innerHTML="Por favor, escribe un correo electrónico válido";
     email.focus();
     return false;
   }
+  else {
+    document.getElementById("mail").innerHTML=""
+   }
 
   if (telefono.value === "") {
     document.getElementById("tel").innerHTML="Por favor, ingrese un numero telefonico";
@@ -31,30 +37,34 @@ document.getElementById("formulario").addEventListener("submit", (e) => {
     return false;
   }
 
-  if(!telefonoValido(telefono.value)){
+  else if(!telefonoValido(telefono.value)){
     document.getElementById("tel").innerHTML="Numero de telefono invalido";
     telefono.focus();
     return false;
   }
+  else{
+    document.getElementById("tel").innerHTML=""
+   }
 
-  if (mensaje.value === "") {
+ if (mensaje.value === "") {
     document.getElementById("men").innerHTML="Por favor, deje su mensaje para poder enviar."
       usuario.focus();
       return false;
     }
+    else {
+      document.getElementById("men").innerHTML=""
+     }
 
   
-  return true; //Se pueden enviar los datos del formulario al servidor
+  return (window.location.href = "registrado.html");
 })
 
+const telefonoValido = telefono => {
+  return (/^\d{10}$/.test(telefono)) ;
+  }
 const emailValido = email => {
   return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
 }
 
-const telefonoValido = telefono =>{
-return (/^\d{10}$/.test(valor));
-}
 
-function clean(){
-  document.getElementsByTagName("p").value="";
-}
+
