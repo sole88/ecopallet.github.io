c966e2f33a32be1223539aefe242076a886833a0
lucas marques de paiva <lucasmarques6@hotmail.com>
2022-10-26 20:08:08 -0300

    se agrego funcion en .js para consumir api de form pero no logre que funcione

--- js/app.js
+++ js/app.js
@@ -64,3 +64,26 @@ const telefonoValido = telefono => {
 const emailValido = email => {
   return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
 }
+const $form = document.querySelector('#form')
+$form.addEventListener('submit', handleSubmit)
+
+async function handleSubmit(event) {
+  event.preventDefault()
+  const form = new FormData(this)
+  const response = await fetch(this.action, {
+    method: this.method,
+    body:form,
+    headers:{
+      'Accept': 'application/json'      }
+  })
+
+  if(response.ok){
+    this.reset()
+    swal({
+      title: "Tu mensaje fue enviado!",
+      text: "Gracias por contactarte, nos comunicaremos con vos lo mas pronto posible.",
+      icon: "success",
+    });
+    
+  }
+}
